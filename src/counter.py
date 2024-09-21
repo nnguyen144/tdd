e1440d9a42cc5077c35fd95c030f27b5f0913731
Nathan Nguyen <nnguyen21697@gmail.com>
2024-09-21 00:46:39 -0700

    Fixed counter.py, test_counter.py, and status.py for flake8

--- src/counter.py
+++ src/counter.py
@@ -14,7 +14,7 @@ def create_counter(name):
     app.logger.info(f"Request to create counter: {name}")
     global COUNTERS
     if name in COUNTERS:
-        return {"Message":f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
+        return {"Message": f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
     COUNTERS[name] = 0
     return {name: COUNTERS[name]}, status.HTTP_201_CREATED
 
@@ -23,8 +23,8 @@ def update_counter(name):
     """Update a counter"""
     app.logger.info(f"Request to update counter: {name}")
     global COUNTERS
-    if not name in COUNTERS:
-        return {"Message":f"Counter {name} does not exist"}, status.HTTP_404_NOT_FOUND
+    if name not in COUNTERS:
+        return {"Message": f"Counter {name} does not exist"}, status.HTTP_404_NOT_FOUND
     COUNTERS[name] += 1
     return {name: COUNTERS[name]}, status.HTTP_200_OK
 
@@ -33,6 +33,6 @@ def read_counter(name):
     """Read a counter"""
     app.logger.info(f"Request to read counter: {name}")
     global COUNTERS
-    if not name in COUNTERS:
-        return {"Message":f"Counter {name} does not exist"}, status.HTTP_404_NOT_FOUND
+    if name not in COUNTERS:
+        return {"Message": f"Counter {name} does not exist"}, status.HTTP_404_NOT_FOUND
     return {name: COUNTERS[name]}, status.HTTP_200_OK
